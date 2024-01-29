712724fb6d22ee4f5c0d300e92e94202245e9014
NadeemNisham <nadeembois2004@gmail.com>
2024-01-30 00:45:46 +0530

    initial commit

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,6 +1,7 @@
 import { webpackBundler } from '@payloadcms/bundler-webpack' // bundler-import
 import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
 import { payloadCloud } from '@payloadcms/plugin-cloud'
+// import formBuilder from '@payloadcms/plugin-form-builder'
 import nestedDocs from '@payloadcms/plugin-nested-docs'
 import redirects from '@payloadcms/plugin-redirects'
 import seo from '@payloadcms/plugin-seo'
@@ -28,6 +29,7 @@ import { Header } from './globals/Header'
 import { Settings } from './globals/Settings'
 import { priceUpdated } from './stripe/webhooks/priceUpdated'
 import { productUpdated } from './stripe/webhooks/productUpdated'
+
 
 const generateTitle: GenerateTitle = () => {
   return 'My Store'
@@ -120,6 +122,7 @@ export default buildConfig({
     },
   ],
   plugins: [
+    // formBuilder({}),
     stripePlugin({
       stripeSecretKey: process.env.STRIPE_SECRET_KEY || '',
       isTestKey: Boolean(process.env.PAYLOAD_PUBLIC_STRIPE_IS_TEST_KEY),
